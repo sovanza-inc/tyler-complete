f05b4a32efc7ef11f97b470f0bd27fdd9912f5ba
dev_ahmad05 <mahmadcoder>
2025-04-07 17:51:31 +0500

    All done

--- Tyler-Backend/server.js
+++ Tyler-Backend/server.js
@@ -125,7 +125,7 @@ app.get('/api/files/:id/table', async (req, res) => {
     }
 });
 
-// Update file endpoint of update file
+// Update file endpoint 
 app.put('/api/files/:id', upload.single('file'), async (req, res) => {
     try {
         const { id } = req.params;
